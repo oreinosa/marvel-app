8b4bce9a920795da971737b95e6e52f1353df9e5
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-25 18:37:22 -0600

    generated core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
