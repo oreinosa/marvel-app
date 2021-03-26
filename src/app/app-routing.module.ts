e1f24fde4f0afc437df8f6628245e9d53c0e1b56
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-25 18:44:44 -0600

    -added /not-found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: 'characters', loadChildren: () => import('./characters/characters.module').then(m => m.CharactersModule) },
   { path: 'comics', loadChildren: () => import('./comics/comics.module').then(m => m.ComicsModule) },
   { path: 'stories', loadChildren: () => import('./stories/stories.module').then(m => m.StoriesModule) },
-  { path: 'bookmarks', loadChildren: () => import('./bookmarks/bookmarks.module').then(m => m.BookmarksModule) }
+  { path: 'bookmarks', loadChildren: () => import('./bookmarks/bookmarks.module').then(m => m.BookmarksModule) },
+  { path: "**", redirectTo: "not-found" }
 ];
 
 @NgModule({
