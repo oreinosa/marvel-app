e1a9eb3a7156f863722bc2f608ffc2ebff429b86
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-25 18:36:08 -0600

    -generated initial modules: shared, core, characters, comics, stories, bookmarks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', loadChildren: () => import('./characters/characters.module').then(m => m.CharactersModule) },
+  { path: 'comics', loadChildren: () => import('./comics/comics.module').then(m => m.ComicsModule) },
+  { path: 'stories', loadChildren: () => import('./stories/stories.module').then(m => m.StoriesModule) },
+  { path: 'bookmarks', loadChildren: () => import('./bookmarks/bookmarks.module').then(m => m.BookmarksModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
