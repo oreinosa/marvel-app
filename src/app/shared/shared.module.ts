acf22e7536ee8a0af0d087447526bba5982b57a8
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-26 13:19:38 -0600

    -import/export material modules in shared module -import/export common, router, and forms modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,94 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { LayoutModule } from '@angular/cdk/layout';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatTableModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    ScrollingModule,
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatTableModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    ScrollingModule,
   ]
 })
 export class SharedModule { }
