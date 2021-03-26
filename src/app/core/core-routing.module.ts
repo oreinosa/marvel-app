2880af73bfaa648d5029d7115e759d1cd2805d94
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-25 18:44:35 -0600

    added /about, /not-found, and / routers

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent }
+  { path: "about", component: AboutComponent },
+  { path: "not-found", component: NotFoundComponent },
+  { path: "", pathMatch: "full", component: HomeComponent },
 ];
 
 @NgModule({
