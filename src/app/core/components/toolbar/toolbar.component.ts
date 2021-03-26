07cd90f997fecbd400fd6830a7a80cc191a129a0
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-26 10:42:11 -0600

    -created toolbar with responsive design. -listening to screenSize to determine which view to show -created desktop and mobile view to hide links and show sidenav button -included actions about and bookmarks

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Link } from 'src/app/shared/models/link';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @Input() screenSize: number = 0;
+  @Input() links: Link[] = [];
+  @Output('toggle') toggleEmitter = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleSidenav() {
+    this.toggleEmitter.emit();
+  }
+
 
 }
