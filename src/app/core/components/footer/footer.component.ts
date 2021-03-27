5f74f8d136f8a9ca9a8f5d122b2c4f39e2802292
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-03-26 19:50:36 -0600

    -added screenSize and links properties for footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Link } from 'src/app/shared/models/link';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  @Input() screenSize: number = 0;
+  @Input() links: Link[] = [];
   constructor() { }
 
   ngOnInit(): void {
