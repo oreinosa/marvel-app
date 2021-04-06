d6dbfbeff060806d8e85c857d2a3e1631c92376e
Oscar Reinosa <oscar.reinosa96@gmail.com>
2021-04-05 19:17:33 -0600

    -added apiKey and apiURL to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "6587e031b1e5b4e0289c1d5b0ba29d4d",
+  apiBaseURL: "https://gateway.marvel.com/v1/public"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "6587e031b1e5b4e0289c1d5b0ba29d4d",
+  apiBaseURL: "https://gateway.marvel.com/v1/public"
 };
 
 /*
